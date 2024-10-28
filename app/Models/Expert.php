70587279aaa5951584382883db07cc4a8892913b
JEKFROZ11 <ipb2023bintang@apps.ipb.ac.id>
2024-10-28 17:31:58 +0700

    pesan commit

---
+++ app/Models/Expert.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Expert extends Model
+{
+    //
+    use HasFactory;
+
+    protected $fillable = [
+        'specialization',
+        'consultation_price',
+        'user_id',
+    ];
+
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
+
+}
