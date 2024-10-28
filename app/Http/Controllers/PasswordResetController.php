e03e87f021670726251fae20afcea1e2902d8e37
Hermawan S Sarjito <hermawansent@gmail.com>
2024-10-28 19:40:13 +0700

    refactor: password reset

--- app/Http/Controllers/PasswordResetController.php
+++ app/Http/Controllers/PasswordResetController.php
@@ -3,8 +3,10 @@
 namespace App\Http\Controllers;
 
 use App\Mail\ResetPasswordLink;
+use App\Models\User;
 use Illuminate\Foundation\Bootstrap\LoadEnvironmentVariables;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\URL;
 
@@ -14,7 +16,7 @@ class PasswordResetController extends Controller
     {
         $data = $request->validate(['email' => 'required']);
 
-       
+
         $url = URL::temporarySignedRoute('password.reset', now()->addMinute(30), ['email' => $request->email]);
         # $url = str_replace(env('app_url', env('FRONTEND_URL')));
 
@@ -22,6 +24,21 @@ class PasswordResetController extends Controller
 
         return response()->json([
             'message' => 'Link reset password sudah dikirim ke email-mu!'
+        ]);
+    }
+
+    public function reset(Request $request){
+        $request->validate([
+            'password' => 'required'
+        ]);
+        $cari = User::where('email', $request->email)->first();
+
+        $cari->update([
+            'password' => Hash::make($request->password)
+        ]);
+
+        return response()->json([
+            'message' => 'Password telah diubah'
         ]);
     }
 }
