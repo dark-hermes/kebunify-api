5dc8b61b69551feceb5bf5354f1c4e542df74303
Hermawan S Sarjito <hermawansent@gmail.com>
2024-10-28 18:31:45 +0700

    refactor: bring back expert model for assignment

--- app/Models/Expert.php
+++ app/Models/Expert.php
@@ -7,62 +7,17 @@ use Illuminate\Database\Eloquent\Model;
 
 class Expert extends Model
 {
+    //
     use HasFactory;
 
     protected $fillable = [
-        'expert_specialization_id',
+        'specialization',
+        'consultation_price',
         'user_id',
-        'start_year',
-        'consulting_fee',
-        'discount',
-        'bio',
     ];
 
-    protected $appends = [
-        'fee_after_discount',
-        'years_of_experience',
-    ];
-
-    protected static function boot()
-    {
-        parent::boot();
-
-        static::created(function ($model) {
-            $model->user->syncRoles('expert');
-        });
-
-        static::updated(function ($model) {
-            $oldUser = $model->getOriginal('user_id');
-            $newUser = $model->user_id;
-
-            if ($oldUser !== $newUser) {
-                User::find($oldUser)->syncRoles('user');
-                User::find($newUser)->syncRoles('expert');
-            }
-        });
-
-        static::deleted(function ($model) {
-            $model->user->syncRoles('user');
-        });
-    }
-
-    public function getFeeAfterDiscountAttribute()
-    {
-        return $this->consulting_fee - ($this->consulting_fee * $this->discount / 100);
-    }
-
-    public function getYearsOfExperienceAttribute()
-    {
-        return now()->year - $this->start_year;
-    }
-
-    public function user()
-    {
+    public function user(){
         return $this->belongsTo(User::class);
     }
 
-    public function specialization()
-    {
-        return $this->belongsTo(ExpertSpecialization::class, 'expert_specialization_id');
-    }
 }
