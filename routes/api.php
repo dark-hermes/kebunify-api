b27a6110150080aa9149adf34709a783aa627162
Reyv <151007888+guuzenn@users.noreply.github.com>
2024-10-28 20:48:36 +0700

    reset fix

--- routes/api.php
+++ routes/api.php
@@ -13,14 +13,15 @@ use App\Http\Controllers\VerificationEmailController;
 
 Route::group(['middleware' => 'guest'], function () {
     Route::post('/login',  [AuthController::class, 'login'])->name('login')->middleware('throttle:6,1');
+
+    Route::post('/email/password', [PasswordResetController::class, 'setResetLinkEmail']);
+    Route::post('/email/password-reset', [PasswordResetController::class, 'reset'])->middleware('signed')->name('password.reset');
 });
+
 
 Route::group(['middleware' => 'auth:sanctum'], function () {
     Route::post('logout', [AuthController::class, 'logout'])->name('logout');
     Route::get('me',     [AuthController::class, 'me'])->name('me');
-
-    Route::post('/email/password', [PasswordResetController::class, 'setResetLinkEmail']);
-    Route::post('/email/password-reset', [PasswordResetController::class, 'reset'])->middleware('signed')->name('password.reset');
 
     Route::post('email/verification-notification', [VerificationEmailController::class, 'send'])->middleware('throttle:6,1')->name('verification.send');
     Route::get('email/verify', [VerificationEmailController::class, 'verify'])->name('verification.verify');
