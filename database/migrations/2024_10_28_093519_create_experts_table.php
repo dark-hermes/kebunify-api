822abf3d7cc64259fe76ae7e98887ea60b44747f
Hermawan S Sarjito <hermawansent@gmail.com>
2024-11-09 02:17:55 +0700

    feat: cors config

--- database/migrations/2024_10_28_093519_create_experts_table.php
+++ database/migrations/2024_10_28_093519_create_experts_table.php
@@ -20,6 +20,7 @@ return new class extends Migration
             $table->unsignedTinyInteger('discount')->default(0);
             $table->text('bio')->nullable();
             $table->boolean('is_active')->default(true);
+            $table->softDeletes();
             $table->timestamps();
         });
     }
