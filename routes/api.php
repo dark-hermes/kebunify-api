882f0982163680008419356d281519e5d1f92475
Hermawan S Sarjito <hermawansent@gmail.com>
2024-11-09 09:13:50 +0700

    experiment: setting cors middleware

--- routes/api.php
+++ routes/api.php
@@ -12,13 +12,6 @@ use App\Http\Controllers\PasswordResetController;
 use App\Http\Controllers\VerificationEmailController;
 use App\Http\Controllers\ExpertSpecializationController;
 
-
-header("Cache-Control: no-cache, must-revalidate");
-header("Expires: Mon, 26 Jul 1997 05:00:00 GMT");
-header('Access-Control-Allow-Origin:  *');
-header('Access-Control-Allow-Methods:  POST, GET, OPTIONS, PUT, DELETE');
-header('Access-Control-Allow-Headers:  Content-Type, X-Auth-Token, Origin, Authorization');
-
 Route::group(['middleware' => 'guest'], function () {
     Route::post('/login',  [AuthController::class, 'login'])->name('login')->middleware('throttle:6,1');
 });
