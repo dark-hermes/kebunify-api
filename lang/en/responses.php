55f7fb100e89a37e636b4f90e7548cf38b6f2c6b
Hermawan S Sarjito <hermawansent@gmail.com>
2024-11-13 00:12:19 +0700

    feat: localization

---
+++ lang/en/responses.php
@@ -0,0 +1,42 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    'index' => [
+        'success' => ':resource successfully fetched.',
+        'failed' => 'Failed to fetch :resource.',
+    ],
+    'store' => [
+        'success' => ':resource successfully created.',
+        'failed' => 'Failed to create :resource.',
+    ],
+    'show' => [
+        'success' => ':resource successfully fetched.',
+        'failed' => 'Failed to fetch :resource.',
+    ],
+    'update' => [
+        'success' => ':resource successfully updated.',
+        'failed' => 'Failed to update :resource.',
+    ],
+    'destroy' => [
+        'success' => ':resource successfully deleted.',
+        'failed' => 'Failed to delete :resource.',
+    ],
+    'upload' => [
+        'success' => ':resource successfully uploaded.',
+        'failed' => 'Failed to upload :resource.',
+    ],
+    'remove' => [
+        'success' => ':resource successfully removed.',
+        'failed' => 'Failed to remove :resource.',
+    ],
+    'follow' => [
+        'success' => 'Successfully followed :resource.',
+        'failed' => 'Failed to follow :resource.',
+    ],
+    'unfollow' => [
+        'success' => 'Successfully unfollowed :resource.',
+        'failed' => 'Failed to unfollow :resource.',
+    ],
+];
