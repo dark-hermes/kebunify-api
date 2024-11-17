22b786562ce9e234eece8051ee62d50deef354dc
Hermawan S Sarjito <hermawansent@gmail.com>
2024-11-17 17:37:01 +0700

    feat: additional expert resource

--- app/Models/ExpertRating.php
+++ app/Models/ExpertRating.php
@@ -6,5 +6,26 @@ use Illuminate\Database\Eloquent\Model;
 
 class ExpertRating extends Model
 {
-    //
+    protected $fillable = [
+        'user_id',
+        'expert_id',
+        'consultation_id',
+        'rating',
+        'comment'
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function expert()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function consultation()
+    {
+        return $this->belongsTo(Consultation::class);
+    }
 }
